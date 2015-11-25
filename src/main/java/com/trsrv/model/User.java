ac0005ef0d566a6c5106aa802af25ebb81162351
Mursan <sandip.murmu@gmail.com>
2015-11-25 15:24:05 +0530

    updated now

--- src/main/java/com/trsrv/model/User.java
+++ src/main/java/com/trsrv/model/User.java
@@ -1,6 +1,5 @@
 package com.trsrv.model;
 
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
--- src/test/java/com/trsrv/model/TransrvTest.java
+++ src/test/java/com/trsrv/model/TransrvTest.java
@@ -1,8 +1,5 @@
 package com.trsrv.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
